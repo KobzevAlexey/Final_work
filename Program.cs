0e5a8679e4b084940a4b526d8c0c973f2d372145
Alexey <kabzone23@mail.ru>
2022-11-11 17:28:00 +0300

    Добавил кривую функцию

--- Program.cs
+++ Program.cs
@@ -2,3 +2,25 @@
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
+using System;
+using static System.Console;
+
+string[] array = new string[4] { "hello", "2", "world", ":-)" };
+var str = string.Join(" ", array);
+WriteLine("[{0}]", string.Join(", ", array));
+WriteLine();
+WriteLine("[{0}]", string.Join(", ", GetArray(array)));
+
+string[] GetArray(string[] array)
+{
+    //string result = string.Empty;
+    string[] arr = new string[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        if (array[i].Length <= 3)
+            arr[i] = array[i];
+
+        // result = Convert.ToString(array[i]);
+    }
+    return arr;
+}
